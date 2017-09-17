00554743cdb2d8af0670174d8ac6d1684d1b7fc7
Jae Lee <ksy903@gmail.com>
2017-09-17 11:58:09 -0500

    getting username php file

---
+++ getUserName.php
@@ -0,0 +1,20 @@
+<?php
+
+	$connection = mysqli_connect("localhost", "root", "", "wordstory");
+	if(mysqli_connect_errno())
+	{
+		echo("console.log(db connection failed);");
+	}
+	else{
+		$user_id = $_POST['user_id'];
+		$query = "SELECT `username` FROM `account` user_id = '$user_id';";
+		$sendPost = mysqli_query($connection, $query);
+		if(!$sendPost){
+            echo("Post was not posted sucessfully");
+        }
+        else{
+			echo(mysqli_fetch_array($sendPost)['username']);
+		}
+	}
+
+?>

cb930ba18e2fb11098d5af5e142a827dfe0638f0
Jordan Taylor <jordanfjtaylor@gmail.com>
2017-09-17 11:57:02 -0500

    Changed js files, added some community features

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showWrite(){
 	document.getElementById('Community_nav').className = 'nav-item';
 	document.getElementById('Profile_nav').className = 'nav-item';
 	document.getElementById('About_nav').className = 'nav-item';
-	
+
 	document.getElementById("write").style.display = 'block';
 	document.getElementById("Community").style.display = 'none';
 	document.getElementById("Profile").style.display = 'none';
@@ -11,28 +11,59 @@ function showWrite(){
 }
 
 function showCommunity(){
-	
+
 	document.getElementById('write_nav').className = 'nav-item';
 	document.getElementById('Community_nav').className = 'nav-item active';
 	document.getElementById('Profile_nav').className = 'nav-item';
 	document.getElementById('About_nav').className = 'nav-item';
-	
+
 	document.getElementById("write").style.display = 'none';
 	document.getElementById("Community").style.display = 'block';
 	document.getElementById("Profile").style.display = 'none';
 	document.getElementById("About").style.display = 'none';
-	
-	getAllPosts();
+	document.getElementById("comStoryBtn1").story = "This is the story of the modal";
+	$('#storyModal').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget) // Button that triggered the modal
+	  var storyText = button.data('story') // Extract info from data-* attributes
+	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	  var modal = $(this)
+	  modal.find('.modal-title').text('This story info')
+	  modal.find('.modal-body input').val(storyText)
+	})
+	//getAllPosts();
+	//
+	// Order
+	// num/postid/, num/topic, num/text, num/points, num/timestamp (yyyy-mm-dd military_time);
+	/*var POSTS = {["postid: 1, topic:Family, points:5, timestamp:2017-09-17"]};
+	int numPosts = POSTS.size();
 	console.log(POSTS);
+
+	for(int i = 0; i < numPosts; i++) {
+				// fill buttons
+				var check = document.getElementsById("comStoryBtn" + i);
+				//Don't do this if the elment doesn't exist
+				if(check.length == 1) {
+					var btn = check[0];
+					for(int valueIndex = 0; valueIndex < 5; valueIndex++) {
+						// Fill the modal & btn with information
+						btn.innerHTML = POSTS[i]["postid"];
+
+
+					}
+				}
+	}*/
+
 }
 
+
 function showProfile(){
-	
+
 	document.getElementById('write_nav').className = 'nav-item';
 	document.getElementById('Community_nav').className = 'nav-item';
 	document.getElementById('Profile_nav').className = 'nav-item active';
 	document.getElementById('About_nav').className = 'nav-item';
-	
+
 	document.getElementById("write").style.display = 'none';
 	document.getElementById("Community").style.display = 'none';
 	document.getElementById("Profile").style.display = 'block';
@@ -44,7 +75,7 @@ function showAbout(){
 	document.getElementById('Community_nav').className = 'nav-item';
 	document.getElementById('Profile_nav').className = 'nav-item';
 	document.getElementById('About_nav').className = 'nav-item active';
-	
+
 	document.getElementById("write").style.display = 'none';
 	document.getElementById("Community").style.display = 'none';
 	document.getElementById("Profile").style.display = 'none';
